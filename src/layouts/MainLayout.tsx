3f621d214f97547184e627bbacc6828b69453f03
William Zapata <zapataw59@gmail.com>
2025-05-15 16:07:51 -0500

    DEV-2 update: MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,13 @@
-import type { ReactNode } from 'react';
+import { Outlet } from 'react-router';
 import Header from "../components/ui/Header.tsx";
 
-interface MainLayoutProps {
-    children: ReactNode;
-}
-
-export default function MainLayout({ children }: MainLayoutProps) {
-    return <>
-        <Header/>
-        <div className={"max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center"}>
-            {children}
-        </div>
-    </>
+export default function MainLayout() {
+    return (
+        <>
+            <Header/>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <Outlet/>
+            </div>
+        </>
+    );
 }
